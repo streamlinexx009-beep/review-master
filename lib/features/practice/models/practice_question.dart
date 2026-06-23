a035ca1c9d9cb2bb2b52ced285f724e917318a64
streamlinexx009-beep <streamlinexx009@gmail.com>
2026-06-23 10:31:24 +0800

    Allow practice questions without exposed answers

--- lib/features/practice/models/practice_question.dart
+++ lib/features/practice/models/practice_question.dart
@@ -19,18 +19,15 @@ class PracticeQuestion {
     this.optionD,
   });
 
-  factory PracticeQuestion.fromMap(
-    Map<String, dynamic> map,
-  ) {
+  factory PracticeQuestion.fromMap(Map<String, dynamic> map) {
     return PracticeQuestion(
-      id: map['id'],
-      question: map['question'],
-      optionA: map['option_a'],
-      optionB: map['option_b'],
-      optionC: map['option_c'],
-      optionD: map['option_d'],
-      correctAnswer:
-          map['correct_answer'],
+      id: map['id']?.toString() ?? '',
+      question: map['question']?.toString() ?? '',
+      optionA: map['option_a']?.toString() ?? '',
+      optionB: map['option_b']?.toString() ?? '',
+      optionC: map['option_c']?.toString(),
+      optionD: map['option_d']?.toString(),
+      correctAnswer: map['correct_answer']?.toString() ?? '',
     );
   }
 }
